8cdd98659c67f0bdb950154242c5c9c3f04092b4
Aadish Shibu <144556449+Aadish680@users.noreply.github.com>
2025-02-11 11:21:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,15 +4,13 @@ const student = {
   name: "John"
 };
 
-// Function to get all keys from the object
+
 function getKeys(obj) {
   return Object.keys(obj);
 }
 
-// Example test cases
-console.log(getKeys(student)); // Output: ["name"]
+console.log(getKeys(student)); 
 
-// Additional tests
 const student2 = {
   name: "Alice",
   age: 22,
@@ -20,5 +18,3 @@ const student2 = {
 };
 console.log(getKeys(student2)); // Output: ["name", "age", "city"]
 
-const emptyObj = {};
-console.log(getKeys(emptyObj)); // Output: []
