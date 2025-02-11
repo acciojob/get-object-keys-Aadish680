4ab475a3c00fccb4c3765be020ab1f042282cf9e
Aadish Shibu <144556449+Aadish680@users.noreply.github.com>
2025-02-11 11:42:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,15 +3,14 @@
 const student = {
   name: "John",
   age: 22,
-	city: "Ahmedabad"
+  city: "Ahmedabad"
 };
 
-
-function getKeys(obj) {
-  return Object.keys(obj);
+Object.prototype.getKeys=function(){
+	return.Object.keys(this);
 }
 
-console.log(getKeys(student)); 
+console.log(student.getKeys); 
 
 
 
